9c72a9f8bfe20b513e9bb725bb9d5530c2ebd36b
DudchenkoViktor <viktordudcenko525@gmail.com>
2025-04-16 18:39:12 +0000

    Fix: working HTTP server implementation

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,6 @@
-# Stage 1: Clone and build
-FROM alpine:latest AS builder
-
-RUN apk add --no-cache git build-base autoconf automake
-WORKDIR /src
-RUN git clone https://github.com/DudchenkoViktor/DevOps2.git . && \
-    git checkout branchHTTPserver && \
-    autoreconf -fi && \
-    ./configure && \
-    make
-
-# Stage 2: Runtime
 FROM alpine:latest
-
-RUN apk add --no-cache libstdc++
-COPY --from=builder /src/program /app/
-COPY --from=builder /src/opp/ /app/opp/
+RUN apk add --no-cache gcompat libstdc++ build-base
 WORKDIR /app
-EXPOSE 8080
-CMD ["./program"]
+COPY . .
+RUN g++ -o server main.cpp -std=c++11 -lpthread
+CMD ["/app/server"]
--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
-#include "opp/trigonometry.h"
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <csignal>
+#include <cstring>
 
 int main() {
-    Trigonometry trig;
-    double x = 0.5;  // Пример значения |x| < 1
-    int n = 3;       // Количество элементов ряда
-    std::cout << "Result: " << trig.FuncA(x, n) << std::endl;
-    return 0;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in address{};
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    listen(server_fd, 10);
+
+    std::cout << "HTTP Server started on port 8080" << std::endl;
+
+    while(true) {
+        int client = accept(server_fd, nullptr, nullptr);
+        const char* response = 
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n\r\n"
+            "<h1>Server is working!</h1>";
+        write(client, response, strlen(response));
+        close(client);
+    }
 }
